f29b95d8d44c5f7d27985f4358855c7a0c047718
arthur-andraade <arthurandradenf@gmail.com>
2020-09-28 18:59:29 -0300

    Analisador léxico

---
+++ Compilador_Pascal/analisador_lexico/codigo.pas
@@ -0,0 +1,11 @@
+program primos;
+    var    
+        numero,i:integer;   
+        primo:boolean;  
+        
+    begin   
+        writeln(' insira o numero'); 
+        readln (numero); 
+        primo:=true; 
+        i:=2; 
+        end.  
